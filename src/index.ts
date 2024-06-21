403c95e71748b2cd09a673343ed93117cb9a7688
nani-samireddy <nanisamireddy05@gmail.com>
2024-06-21 19:24:07 +0530

    chore: added connectDB method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { connectDB } from './services/database-service';
+
+const mongoURL = process.env.MONGO_URI;
 
 // Get the env variables.
 const PORT = process.env.PORT || 4000;
@@ -14,10 +17,11 @@ app.use(cookieParser());
 app.use(express.json());
 
 // Add the routes.
-app.get("/", (req, res) => {
-  res.send("MVEP API");
+app.get('/', (req, res) => {
+  res.send('MVEP API');
 });
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server started at ${PORT}`);
 });
