1b707c876805e5735a39421a0e084b347c5bf1ee
nani-samireddy <nanisamireddy05@gmail.com>
2024-06-21 16:14:15 +0530

    chore: added folder structure and initial packages

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+// Get the env variables.
+const PORT = process.env.PORT || 4000;
+
+// Define the express app.
 const app = express();
 
+// Add the middlewares.
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+// Add the routes.
 app.get("/", (req, res) => {
   res.send("MVEP API");
 });
 
-app.listen(4000);
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
